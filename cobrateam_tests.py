bf157e20955962e2d59a9a1c02d42bea5c004d99
Francisco Souza <francisco@franciscosouza.net>
2011-05-16 11:42:20 -0300

    Unused import

--- cobrateam_tests.py
+++ cobrateam_tests.py
@@ -1,16 +1,14 @@
-import os
 import index
 import unittest
-import tempfile
 
 class CobraTeamTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = index.app.test_client()
-            
+
     def test_index(self):
         request = self.app.get('/')
         assert 'Projects' in request.data
-        
+
 if __name__ == '__main__':
     unittest.main()
