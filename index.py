04ce6f86d1a5e862f9bcb467080bd45d00342822
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-05-16 03:29:56 -0300

    Add github api to get repositories and members

--- index.py
+++ index.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
+from urllib2 import urlopen
+import simplejson
 
 app = Flask("CobraTeam Website")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    members = get_members()
+    projects = get_projects()
+    return render_template("index.html", members=members, projects=projects)
+
+def get_members():
+    users = simplejson.load(urlopen('https://github.com/api/v2/json/organizations/cobrateam/public_members'))
+    members = []
+    for user in users['users']:
+        members.append((user['name'], user['login']))
+
+    return members
+
+def get_projects():
+    repositories = simplejson.load(urlopen('https://github.com/api/v2/json/organizations/cobrateam/public_repositories'))
+    projects = []
+    for project in repositories['repositories']:
+        projects.append((project['name'], project['url']))
+
+    return projects
 
 if __name__ == "__main__":
     app.run(debug=True)
