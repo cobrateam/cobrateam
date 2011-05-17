7454149d480e35506462fa3b253651826569533b
Francisco Souza <francisco@franciscosouza.net>
2011-05-16 22:04:24 -0300

    Added a decorator for HTML minifying

--- cobrateam_tests.py
+++ cobrateam_tests.py
@@ -10,5 +10,16 @@ class CobraTeamTestCase(unittest.TestCase):
         request = self.app.get('/')
         assert 'Projects' in request.data
 
+    def test_should_minify_the_rendered_content_from_view_using_a_decorator(self):
+        from decorators import minified_response
+
+        @index.app.route("/bla-bla-bla")
+        @minified_response
+        def bla_bla_bla():
+            return '<html>    <body>Hello world</body>    </html>'
+
+        response = self.app.get('/bla-bla-bla')
+        self.assertEqual('<html><body>Hello world</body></html>', response.data)
+
 if __name__ == '__main__':
     unittest.main()
