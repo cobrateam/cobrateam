b7546d2e6b5ed23808e83f53e00f3c15c1d3adae
Evandro Flores <eof@eof.com.br>
2012-03-15 11:40:28 -0300

    new site layout

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ def get_members():
     users = simplejson.load(urlopen('https://github.com/api/v2/json/organizations/cobrateam/public_members'))
     members = []
     for user in sorted(users['users'], key=lambda x: x['name']):
-        members.append((user['name'], user['login']))
+        members.append((user['name'], user['login'], user['gravatar_id']))
 
     return members
 
